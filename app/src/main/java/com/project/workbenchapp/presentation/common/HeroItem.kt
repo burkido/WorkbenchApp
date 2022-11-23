6c8064def805bc773f981b9e24ce80d255d34351
Burak Karaduman <karadumanburak000@gmail.com>
2022-11-23 20:50:44 +0300

    Item layouts added

---
+++ app/src/main/java/com/project/workbenchapp/presentation/common/HeroItem.kt
@@ -0,0 +1,8 @@
+package com.project.workbenchapp.presentation.common
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HeroItem() {
+
+}
--- app/src/main/java/com/project/workbenchapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/project/workbenchapp/presentation/screens/home/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,17 +16,23 @@ fun HomeScreen(
 ) {
 
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
-    Scaffold(
-        topBar = {
-            HomeTopBar(onSearchClicked = {})
-        }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-        ) {
 
-        }
-    }
+//    Scaffold(
+//        topBar = {
+//            HomeTopBar(onSearchClicked = {})
+//        },
+//        content = {
+//            ListContent(
+//
+//            )
+//        }
+//    ) { padding ->
+//        Column(
+//            modifier = Modifier
+//                .padding(padding)
+//        ) {
+//
+//        }
+//    }
 
 }
